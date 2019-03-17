13a4d248d5647d404f3c2c2facc010cbe2960fb9
admin <18235948059@163.com>
2019-03-17 09:25:28 +0800

    tianjia

---
+++ gitdemo/src/main/java/com/itcast/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("3333");
+        System.out.println("4444");
+        System.out.println("5555");
+    }
+}
